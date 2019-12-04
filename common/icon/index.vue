9a46f8d9a2525f5d14376f8b46b7c0c514479417
wangshi <wangshi@xiaoxiangyoupin.cn>
2019-12-04 15:10:02 +0800

    新增自定义组件

---
+++ common/icon/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <i class="ta-icon" :class="'ion-' + name" @click="emitClick()"></i>
+</template>
+
+<script>
+export default {
+  name: 'TaIcon',
+
+  props: ['name'],
+
+  methods: {
+    emitClick() {
+      this.$emit('click')
+    },
+  },
+}
+</script>
+
